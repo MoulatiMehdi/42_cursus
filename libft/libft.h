d4447a4cc4e4b51e9aa58601688cb8c171c83e78
MoulatiMehdi <moulati.mehdi@gmail.com>
2024-10-24 20:06:15 +0100

    fix conflict string branch

--- libft/libft.h
+++ libft/libft.h
@@ -6,31 +6,31 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:27:34 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/10/24 18:52:51 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/10/24 20:05:13 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				toupper(int c);
-int				tolower(int c);
-int				isalpha(int c);
-int				isdigit(int c);
-int				isalnum(int c);
-int				isascii(int c);
-int				isprint(int c);
-int				strcmp(const char *s1, const char *s2);
-int				memcmp(const void *s1, const void *s2, unsigned long n);
-int				atoi(const char *str);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_strcmp(const char *s1, const char *s2);
+int				ft_memcmp(const void *s1, const void *s2, unsigned long n);
+int				ft_atoi(const char *str);
 
-unsigned long	strlen(const char *s);
-unsigned long	strlcpy(char *dst, const char *src, unsigned long dstsize);
-unsigned long	strlcpy(char *dst, const char *src, unsigned long dstsize);
+unsigned long	ft_strlen(const char *s);
+unsigned long	ft_strlcpy(char *dst, const char *src, unsigned long dstsize);
+unsigned long	ft_strlcpy(char *dst, const char *src, unsigned long dstsize);
 
-char			*strstr(const char *haystack, const char *needle);
-char			*strchr(const char *s, int c);
-char			*strchr(const char *s, int c);
+char			*ft_strstr(const char *haystack, const char *needle);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strchr(const char *s, int c);
 
-void			*memset(void *b, int c, unsigned long len);
-void			*memcpy(void *dst, const void *src, unsigned long n);
-void			*memmove(void *dst, const void *src, unsigned long len);
-void			*memchr(const void *s, int c, unsigned long n);
-void			bzero(void *s, unsigned long n);
+void			*ft_memset(void *b, int c, unsigned long len);
+void			*ft_memcpy(void *dst, const void *src, unsigned long n);
+void			*ft_memmove(void *dst, const void *src, unsigned long len);
+void			*ft_memchr(const void *s, int c, unsigned long n);
+void			ft_bzero(void *s, unsigned long n);
