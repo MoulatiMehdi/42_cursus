b85113889c17ead93f1a316927eb6fd4dfbec998
MoulatiMehdi <moulati.mehdi@gmail.com>
2024-10-24 20:39:16 +0100

    add strncmp and strnstr functions

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,11 +6,11 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:34:56 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/10/24 15:44:42 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/10/24 20:38:41 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, int n)
+int	ft_strncmp(const char *s1, const char *s2, unsigned long n)
 {
 	int	i;
 
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,8 +6,30 @@
 /*   By: mmoulati <mmoulati@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:06:47 by mmoulati          #+#    #+#             */
-/*   Updated: 2024/10/24 16:06:47 by mmoulati         ###   ########.fr       */
+/*   Updated: 2024/10/24 20:36:53 by mmoulati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strnstr(const char *haystack, const char *needle, unsigned long len)
+{
+	int	i;
+	int	j;
 
+	if (!haystack || !needle)
+		return (0);
+	i = 0;
+	while (i < len && haystack[i])
+	{
+		j = 0;
+		while (needle[j])
+		{
+			if (i + j < len || needle[j] != haystack[i + j])
+				break ;
+			j++;
+		}
+		if (needle[j] == haystack[i + j])
+			return ((char *)haystack);
+		i++;
+	}
+	return (0);
+}
